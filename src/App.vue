72d84ebb8ba6d8142a2253d5fef95b6b40517c8d
drissnafii <drissnafi3@gmail.com>
2026-01-29 09:52:27 +0100

    feat: implement todo list functionality with add and display features

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Todo {
+  id: number
+  text: string
+  completed: boolean
+}
+
+const newTodoText = ref('') // for the input
+const todos = ref<Todo[]>([]) // for the list of tasks
+
+// function to add a todo
+const addTodo = () => {
+  if (newTodoText.value.trim() === '') return // Validation simple
+
+  const newTodo: Todo = {
+    id: Date.now(),
+    text: newTodoText.value,
+    completed: false,
+  }
+
+  // push task into the ref using .value
+  todos.value.push(newTodo)
+  newTodoText.value = ''
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <h1>My todolist</h1>
+    <div>
+      <input
+        type="text"
+        v-model="newTodoText"
+        @keyup.enter="addTodo"
+        placeholder="add new task..."
+      />
+      <button @click="addTodo">Add</button>
+    </div>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input type="checkbox" v-model="todo.completed">
+        {{ todo.text }}
+      </li>
+    </ul>
+    <p v-if="todos.length === 0">There is no task for the moment</p>
+  </main>
 </template>
 
 <style scoped></style>
